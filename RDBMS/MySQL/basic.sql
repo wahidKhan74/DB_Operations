0c41dcdb673c87e2df852d95a8cda982bb3f4195
Waheed Khan <wahidXX@xxx.com>
2025-08-29 07:38:56 +0530

    Added Db operations

---
+++ RDBMS/MySQL/basic.sql
@@ -0,0 +1,12 @@
+-- list databases;
+> SHOW databases;
+> show databases;
+
+-- create databse
+> CREATE DATABASE school;
+
+-- Use Database
+> USE school;
+
+-- Show current used db
+> SELECT DATABASE();
